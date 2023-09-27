f5543c3ec18c37467f37f00ad28e18790d8006e7
SigurdGullev <sigurdfabergullev@gmail.com>
2023-09-27 14:53:22 +0200

    sim

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
-st.title('My Streamlit App')
-st.write("Welcome to my Streamlit application!")
+# Simulate a Dataset
+def simulate_data():
+    np.random.seed(42)  # Seed for reproducibility
+    X = 2 * np.random.rand(100, 1)
+    y = 4 + 3 * X + np.random.randn(100, 1)
+    return X, y
+
+# Linear Regression and Plotting
+def plot_regression(X, y):
+    reg = LinearRegression().fit(X, y)
+    line = reg.coef_ * X + reg.intercept_
+
+    plt.scatter(X, y)
+    plt.plot(X, line, 'r')
+    plt.title("Linear Regression")
+    plt.xlabel("X")
+    plt.ylabel("y")
+    st.pyplot()
+
+st.title("Interactive Regression on Simulated Data")
+
+# Simulate the data
+X, y = simulate_data()
+
+if st.button('Run Regression'):
+    plot_regression(X, y)
+    st.write(f"Equation: y = {reg.coef_[0][0]:.2f}X + {reg.intercept_[0]:.2f}")
