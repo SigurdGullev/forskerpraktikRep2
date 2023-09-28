cba3404619d58c22a2170da3886df1342ce5bedc
- <sigurdfabergullev@gmail.com>
2023-09-28 16:03:31 +0200

    added mediator DAG

--- main.py
+++ main.py
@@ -39,3 +39,31 @@ if st.button('Generate Collider DAG'):
     plot_collider_dag(df)
 
 # ... (other DAGs can follow a similar pattern)
+# Mediator DAG
+def simulate_mediator_data():
+    SIZE = 1000
+    X = np.random.normal(size=SIZE)
+    Z = 1.5 * X + np.random.normal(size=SIZE)
+    e = np.random.normal(size=SIZE)  # noise
+    Y = 2 * Z + e
+    df = pd.DataFrame({'X': X, 'Y': Y, 'Z': Z})
+    return df
+
+def plot_mediator_dag(df):
+    # X -> Z -> Y
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sm.graphics.plot_partregress(endog='Y', exog_i='X', exog_others=[], data=df, ax=ax, obs_labels=False)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    st.pyplot(fig)  # Explicitly pass the figure object to Streamlit
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sm.graphics.plot_partregress(endog='Y', exog_i='X', exog_others=['Z'], data=df, ax=ax, obs_labels=False)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    st.pyplot(fig)
+
+# Button for Mediator DAG
+if st.button('Generate Mediator DAG'):
+    df = simulate_mediator_data()
+    plot_mediator_dag(df)
