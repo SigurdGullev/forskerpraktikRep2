709db6ec9dd5d6ca383abff3bc9c56dbddc081b5
- <sigurdfabergullev@gmail.com>
2023-09-28 15:57:58 +0200

    nye plots

---
+++ backup.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# Simulate a Dataset
+def simulate_data():
+    np.random.seed(42)  # Seed for reproducibility
+    X = 2 * np.random.rand(100, 1)
+    y = 4 + 3 * X + np.random.randn(100, 1)
+    return X, y
+
+def plot_regression(X, y):
+    reg = LinearRegression().fit(X, y)
+    line = reg.coef_ * X + reg.intercept_
+
+    fig, ax = plt.subplots()  # Create figure and axis objects
+    ax.scatter(X, y)
+    ax.plot(X, line, 'r')
+    ax.set_title("Linear Regression")
+    ax.set_xlabel("X")
+    ax.set_ylabel("y")
+    st.pyplot(fig)  # Pass the figure object explicitly
+
+    return reg
+
+st.title("Interactive Regression on Simulated Data")
+
+# Simulate the data
+X, y = simulate_data()
+
+# Display dataset
+st.write("Simulated Dataset:")
+st.write("X values:", X.flatten())
+st.write("Y values:", y.flatten())
+
+if st.button('Run Regression'):
+    reg = plot_regression(X, y)  # Capture the returned regression object
+    st.write(f"Equation: y = {reg.coef_[0][0]:.2f}X + {reg.intercept_[0]:.2f}")
+    st.write("Here's how the regression worked:")
+    st.write("A linear regression model was fitted to the simulated dataset. The regression line represents the best-fit line that minimizes the sum of squared errors between the predicted values and actual values.")
--- main.py
+++ main.py
@@ -1,41 +1,51 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import statsmodels.formula.api as smf
+import statsmodels.graphics.api as sm
 
-# Simulate a Dataset
-def simulate_data():
-    np.random.seed(42)  # Seed for reproducibility
-    X = 2 * np.random.rand(100, 1)
-    y = 4 + 3 * X + np.random.randn(100, 1)
-    return X, y
+st.title("Directed Acyclical Graphs (DAGs)")
 
-def plot_regression(X, y):
-    reg = LinearRegression().fit(X, y)
-    line = reg.coef_ * X + reg.intercept_
+# Collider DAG
+def simulate_collider_data():
+    SIZE = 1000
+    X = np.random.normal(size=SIZE)
+    Y = np.random.normal(size=SIZE)
+    e = np.random.normal(size=SIZE)  # noise
+    Z = 2*X + 1*Y + e
+    df = pd.DataFrame({'X': X, 'Y': Y, 'Z': Z})
+    return df
 
-    fig, ax = plt.subplots()  # Create figure and axis objects
-    ax.scatter(X, y)
-    ax.plot(X, line, 'r')
-    ax.set_title("Linear Regression")
-    ax.set_xlabel("X")
-    ax.set_ylabel("y")
-    st.pyplot(fig)  # Pass the figure object explicitly
+def plot_collider_dag(df):
+    # Y -> Z <- X
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sm.graphics.plot_partregress(endog='Y', exog_i='X', exog_others=[], data=df, ax=ax, obs_labels=False)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    st.pyplot(fig)  # Explicitly pass the figure object to Streamlit
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sm.graphics.plot_partregress(endog='Y', exog_i='X', exog_others=['Z'], data=df, ax=ax, obs_labels=False)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    st.pyplot(fig)
 
-    return reg
+    mod = smf.ols(formula='Y ~ X + Z', data=df)
+    res = mod.fit()
 
-st.title("Interactive Regression on Simulated Data")
+    st.write(res.summary())
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sm.graphics.plot_partregress(endog='Y', exog_i='X', exog_others='Z', data=df, ax=ax, obs_labels=False)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    st.pyplot(fig)
 
-# Simulate the data
-X, y = simulate_data()
+# Button for Collider DAG
+if st.button('Generate Collider DAG'):
+    df = simulate_collider_data()
+    plot_collider_dag(df)
 
-# Display dataset
-st.write("Simulated Dataset:")
-st.write("X values:", X.flatten())
-st.write("Y values:", y.flatten())
-
-if st.button('Run Regression'):
-    reg = plot_regression(X, y)  # Capture the returned regression object
-    st.write(f"Equation: y = {reg.coef_[0][0]:.2f}X + {reg.intercept_[0]:.2f}")
-    st.write("Here's how the regression worked:")
-    st.write("A linear regression model was fitted to the simulated dataset. The regression line represents the best-fit line that minimizes the sum of squared errors between the predicted values and actual values.")
+# ... (other DAGs can follow a similar pattern)
