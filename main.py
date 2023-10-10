ad001b3121dc631538dcff5464a37a8e19bc1928
- <sigurdfabergullev@gmail.com>
2023-10-10 13:27:17 +0200

    muggi

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def plot_collider_dag(df):
     
     fig, ax = plt.subplots(figsize=(8, 6))
     sm.plot_partregress(endog='Y', exog_i='X', exog_others=['Z'], data=df, ax=ax, obs_labels=False)
-    ax.set_xlabel('X')
+    ax.set_xlabel('X muggi')
     ax.set_ylabel('Y')
     #ax.set_xlim([0, 10])
     #ax.set_ylim([0, 10])
