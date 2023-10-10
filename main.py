05c5ab95c17f0a88f0e05c05c83ba90e2c2657fe
- <sigurdfabergullev@gmail.com>
2023-10-10 13:17:19 +0200

    x10

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ with buttons[3]:
 # Collider DAG
 def simulate_collider_data():
     SIZE = 1000
-    X = np.random.uniform(0, 10, SIZE)  # values between 0 and 10
+    X = np.random.uniform(0, 10, SIZE)*10  # values between 0 and 10
     Y = np.random.uniform(0, 10, SIZE)  # values between 0 and 10
     e = np.random.normal(size=SIZE)     # noise
     Z = 2*X + 1 *Y + e
