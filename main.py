95bf4a0ac3055d7bd923aa76416f4ae7ae4227c1
- <sigurdfabergullev@gmail.com>
2023-11-12 16:05:46 +0100

    sd

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ with buttons[3]:
     confounding_button = st.button('Generate Confounding DAG')
 
 # Function to plot with a calculated regression line
-def plot_with_regression_line(df, x_col, y_col, title, scatter_color='#8bcfbd', line_color='black', background_color='#e5e5e5'):
+def plot_with_regression_line(df, x_col, y_col, title, scatter_coslor='#8bcfbd', line_color='black', background_color='#e5e5e5'):
     x = df[x_col]
     y = df[y_col]
     coefficients = np.polyfit(x, y, 1)  # Fit a linear regression model
     regression_line = np.polyval(coefficients, x)  # Calculate the regression line
     
-    fig, ax = plt.subplots(figsize=(6, 6))
+    fig, ax = plt.subplots(figsize=(9, 6))
     fig.patch.set_facecolor(background_color)  # Set the background color
 
     ax.scatter(df[x_col], df[y_col], alpha=0.5, color=scatter_color)
